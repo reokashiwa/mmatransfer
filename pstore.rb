4a71976a935fb3314b5a90218f94da54d31d495b
Hiroki (REO) Kashiwazaki <reo.kashiwazaki@gmail.com>
2017-01-02 22:40:33 +0900

    add delete option to pstore.rb

--- pstore.rb
+++ pstore.rb
@@ -1,46 +1,70 @@
-#!/opt/local/bin/ruby -Ku
+#!/usr/bin/ruby -Ku
 
 require 'optparse'
 require 'pstore'
+require 'highline/import'
 
 opt = OptionParser.new
 OPTS = Hash.new
 OPTS[:indexfile] = "index.db"
+OPTS[:mode] = :show
 opt.on('-i VAL', '--indexfile VAL') {|v| OPTS[:indexfile] = v}
 opt.on('-c', '--csv-output') {|v| OPTS[:output] = "csv" }
 opt.on('-t', '--tsv-output') {|v| OPTS[:output] = "tsv" }
+opt.on('-d', '--delete') {OPTS[:mode] = :delete}
 opt.parse!(ARGV)
 
 db = PStore.new(OPTS[:indexfile])
 db.transaction do
 
-  if File.pipe?(STDIN) then
-    STDIN.each{|line|
-      name = line.chomp
-      case OPTS[:output]
-      when "csv" then
-        format = "%s,%s,%s,%s,%s,%s,%s,%s,%s\n"
-      when "tsv" then
-        format = "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n"
-      end
-      printf(format, 
-             name, db[name]["basename"], db[name]["filesize"],
-             db[name]["ctime"], db[name]["duration"], db[name]["start"], 
-             db[name]["bitrate"], db[name]["width"], db[name]["height"])
-    }
-  else
+  case OPTS[:mode]
+  when :show
+    if File.pipe?(STDIN) then
+      STDIN.each{|line|
+        name = line.chomp
+        case OPTS[:output]
+        when "csv" then
+          format = "%s,%s,%s,%s,%s,%s,%s,%s,%s\n"
+        when "tsv" then
+          format = "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n"
+        end
+        printf(format, 
+               name, db[name]["basename"], db[name]["filesize"],
+               db[name]["ctime"], db[name]["duration"], db[name]["start"], 
+               db[name]["bitrate"], db[name]["width"], db[name]["height"])
+      }
+    else
+      db.roots.each{|name|
+        case OPTS[:output]
+        when "csv" then
+          format = "%s,%s,%s,%s,%s,%s,%s,%s,%s\n"
+        when "tsv" then
+          format = "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n"
+        end
+        printf(format, 
+               name, db[name]["basename"], db[name]["filesize"],
+               db[name]["ctime"], db[name]["duration"], db[name]["start"], 
+               db[name]["bitrate"], db[name]["width"], db[name]["height"])
+      }
+    end
 
+  when :delete
+    targets = Array.new
     db.roots.each{|name|
-      case OPTS[:output]
-      when "csv" then
-        format = "%s,%s,%s,%s,%s,%s,%s,%s,%s\n"
-      when "tsv" then
-        format = "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n"
-      end
-      printf(format, 
-             name, db[name]["basename"], db[name]["filesize"],
-             db[name]["ctime"], db[name]["duration"], db[name]["start"], 
-             db[name]["bitrate"], db[name]["width"], db[name]["height"])
+      targets.push(name) if name =~ Regexp.new(ARGV[0])
     }
+
+    targets.each{|target|
+      if db.root?(target)
+        p target
+        printf("%s\n", db[target])
+        #p 'yes' if HighLine.agree('Do it? [Y/n]')
+        db.delete(target) if HighLine.agree('Do it? [Y/n]')
+      else
+        printf("name %s does not exist.\n", target)
+      end
+    }
+
+  else
   end
 end
